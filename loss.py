51ea66e35f7d69338a34474dc11da44848d5f973
Tianduo Wang <wtianduo@gmail.com>
2020-03-09 21:08:14 +0800

    update loss

--- loss.py
+++ loss.py
@@ -1,7 +1,39 @@
 import torch.nn as nn
+from torch.nn import LogSigmoid
+from sklearn.metrics import log_loss
 
 
-class CustomLoss(nn.Module):
+log_sigmoid = LogSigmoid()
+
+
+class CustomLoss1(nn.Module):
+
+	def __init__(self):
+		super(CustomLoss, self).__init__()
+		
+
+	def forward(self, inputs, targets):
+
+		inputs = inputs.flatten()
+		targets = target.flatten()
+
+		loss = -(targets * log_sigmoid(inputs) + (1 - targets) * log_sigmoid(-inputs))
+		return torch.sum(loss)
+
+
+class CustomLoss2(nn.Module):
+
+	def __init__(self):
+		super(CustomLoss, self).__init__()
+		
+
+	def forward(self, inputs, targets):
+
+		loss = -(targets * log_sigmoid(inputs) + (1 - targets) * log_sigmoid(-inputs))
+		return torch.sum(loss)
+
+
+class CustomLoss3(nn.Module):
 
 	def __init__(self):
 		super(CustomLoss, self).__init__()
