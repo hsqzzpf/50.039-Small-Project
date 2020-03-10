cfe1a7afb46cf3b7f2c07913ffc0a4c1a644dae3
Tianduo Wang <wtianduo@gmail.com>
2020-03-10 18:56:59 +0800

    update loss

--- loss.py
+++ loss.py
@@ -1,3 +1,4 @@
+import torch
 import torch.nn as nn
 from torch.nn import LogSigmoid
 from sklearn.metrics import log_loss
@@ -9,35 +10,46 @@ log_sigmoid = LogSigmoid()
 class CustomLoss1(nn.Module):
 
 	def __init__(self):
-		super(CustomLoss, self).__init__()
+		super(CustomLoss1, self).__init__()
 		
 
 	def forward(self, inputs, targets):
 
 		inputs = inputs.flatten()
-		targets = target.flatten()
+		targets = targets.flatten()
 
-		loss = -(targets * log_sigmoid(inputs) + (1 - targets) * log_sigmoid(-inputs))
+		loss = -(targets * log_sigmoid(inputs) + (1 - targets) * log_sigmoid(1-inputs))
+		print(loss)
 		return torch.sum(loss)
 
 
 class CustomLoss2(nn.Module):
 
 	def __init__(self):
-		super(CustomLoss, self).__init__()
+		super(CustomLoss2, self).__init__()
 		
 
 	def forward(self, inputs, targets):
 
-		loss = -(targets * log_sigmoid(inputs) + (1 - targets) * log_sigmoid(-inputs))
+		loss = -(targets * log_sigmoid(inputs) + (1 - targets) * log_sigmoid(1-inputs))
 		return torch.sum(loss)
 
 
 class CustomLoss3(nn.Module):
 
 	def __init__(self):
-		super(CustomLoss, self).__init__()
+		super(CustomLoss3, self).__init__()
 		
 
 	def forward(self, inputs, targets):
 		return torch.mean((inputs - targets)**2)
+
+if __name__ == "__main__":
+	loss_fn = CustomLoss1()
+
+	y_true = torch.tensor([0., 0., 1., 1.])
+	y_pred = torch.tensor([0.1, 0.1, 0.9, 0.9])
+
+	print(loss_fn(y_pred, y_true))
+	print(log_sigmoid(torch.tensor(0.9)))
+
